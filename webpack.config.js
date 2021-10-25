89f730b1340df6891fac84f82c32c98e350104cf
denisZlobich <sgsdgk9232@gmail.com>
2021-10-25 17:06:19 +0300

    2.1 Installed all the necessary packages. Installed and configured the webpack.

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require(`path`);
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// postcss.config.js
+module.exports = {
+    plugins: {
+        'postcss-preset-env': {
+            browsers: 'last 2 versions',
+        },
+    },
+}
+
+module.exports = {
+    target: `node`,
+    mode: `development`,
+    entry: `./src/main.js`,
+    output: {
+        filename: `bundle.js`,
+        path: path.join(__dirname, `./public`),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/template.html'), // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            // JavaScript
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            // CSS
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+        ],
+    },
+    devtool: `source-map`,
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+      },
+};
