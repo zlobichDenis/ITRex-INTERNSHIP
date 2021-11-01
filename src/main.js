4b70f17c80c2e8a9ce665302ac68547c0935b4d4
denisZlobich <sgsdgk9232@gmail.com>
2021-11-01 12:32:12 +0300

    3. Created and configured the preliminary structure of the project.

--- src/main.js
+++ src/main.js
@@ -1,24 +1,3 @@
-import AuthorizationFormContainer from "./script/view/authorization-form/authorization-form";
-import AuthorizationFormController from "./script/controllers/authorization-form-controller";
-import { ActiveScreen, AuthState } from "./script/common/const";
-import AuthorizationFormModel from "./script/model/authorization-form-model";
 
-
-const rootContainer = document.getElementById('root');
-
-const authorizationFormView = new AuthorizationFormContainer();
-const authorizationFormModel = new AuthorizationFormModel();
-const authorizationFormController = new AuthorizationFormController(authorizationFormModel, authorizationFormView);
-
-
-
-const renderActiveScreen = (activeScreen = ActiveScreen.AUTH) => {
-    switch (activeScreen) {
-        case ActiveScreen.AUTH:
-        authorizationFormController.render(rootContainer);
-    }
-};
-
-renderActiveScreen();
 
 
