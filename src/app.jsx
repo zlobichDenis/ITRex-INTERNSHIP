06329087d496d18920abce14a8fad8af1b8bcafe
denisZlobich <sgsdgk9232@gmail.com>
2021-11-08 18:02:08 +0300

    3.28 Deleted unnecessary files.

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { store } from "./store";
 
-import { AppRouter } from "./routes/index";
+import { store } from "store";
+import { AppRouter } from "routes";
 
 export const App = () => {
     return  (
