ea7d90d48b048c8033535198fe4f84149ce30d0b
denisZlobich <sgsdgk9232@gmail.com>
2021-11-14 22:50:53 +0300

    4.4 Fixed bugs with layout.

--- src/components/CabinetHeader/styles.js
+++ src/components/CabinetHeader/styles.js
@@ -4,6 +4,7 @@ import { colors } from "styles";
 
 export const Header = styled.div`
     display: flex;
+    width: 95%;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
@@ -12,12 +13,10 @@ export const Header = styled.div`
 
     @media (min-width: 1920px) {
         width: 95%;
-        padding: 24px 0;
     }
 
     @media (min-width: 768px) and (max-width: 1919px) {
         width: 90%;
-        padding: 24px 0;
         background-color: transparent;
     }
 `;
@@ -29,21 +28,31 @@ export const UserInfo = styled.div`
 `;
 
 export const UserName = styled.span`
+    display: block;
     font-family: Poppins;
     font-style: normal;
     font-weight: 600;
     font-size: 15px;
     line-height: 130%;
-    color: ${colors.TEXT_TITLE_COLOR}
+    color: ${colors.TEXT_TITLE_COLOR};
+    
+    @media (min-width: 321px) and (max-width: 768px) {
+        display: none;
+    }
 `;
 
 export const UserSpecialty = styled.span`
+    display: block;
     font-family: Poppins;
     font-style: normal;
     font-weight: 500;
     font-size: 13px;
     line-height: 130%;
     color: ${colors.TEXT_COLOR};
+
+    @media (min-width: 321px) and (max-width: 768px) {
+        display: none;
+    }
 `;
 
 export const UserNameWrapper = styled.div`
