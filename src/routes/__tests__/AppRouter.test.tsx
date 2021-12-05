757b449e4c136687f0d3e69baccd72843d938c61
denisZlobich <sgsdgk9232@gmail.com>
2021-12-05 16:52:10 +0300

    Refactoring: -transferred ConnectedRouter component to app

--- src/routes/__tests__/AppRouter.test.tsx
+++ src/routes/__tests__/AppRouter.test.tsx
@@ -2,23 +2,21 @@
 import { Provider } from "react-redux";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { ConnectedRouter } from "connected-react-router";
-import { createBrowserHistory } from "history";
 import "@testing-library/jest-dom";
 
-import { AppScreens, PatientScreens } from "const";
 import { store } from "store";
 import { AppRouter } from "routes";
+import { history } from "store";
+import { ConnectedRouter } from "connected-react-router";
 
 
 describe('router', () => {
   it('should render correct sign in page when click on sign in link', async () => {
     jest.restoreAllMocks();
-    const history = createBrowserHistory()
 
     render(
       <Provider store={store}>
-        <ConnectedRouter history={history} >
+        <ConnectedRouter history={history}>
           <AppRouter />
         </ConnectedRouter>
       </Provider>
@@ -26,9 +24,10 @@ describe('router', () => {
 
     userEvent.click(screen.getByText('Sign In'))
 
-    // userEvent.type(screen.getByRole('email-input'), 'dasha@tut.by');
-    // userEvent.type(screen.getByRole('password-input'), 'dasha@tut.by');
-    // userEvent.click(screen.getByRole('submit-button'));
+    userEvent.type(screen.getByRole('email-input'), 'dasha@tut.by');
+    userEvent.type(screen.getByRole('password-input'), 'dasha@tut.by');
+    userEvent.click(screen.getByRole('submit-button'));
+    userEvent.click(screen.getByRole('submit-button'));
 
     expect(screen.getByTestId('sign-in-form')).toBeInTheDocument()
   })
